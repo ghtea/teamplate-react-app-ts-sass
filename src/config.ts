66789ef0a65e6032f40d2c73de5437c74eac370a
EC2 Default User <ec2-user@ip-172-31-34-36.ap-northeast-2.compute.internal>
2020-11-26 14:51:05 +0000

    deleted config

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export const URL_BACK:string = 'https://template_back.nextwing.me';
-
-export const URL_FRONT:string = 'https://main.d2n0m3wwgzxqlu.amplifyapp.com';
-
-
-export const STATUS:string = "developing"; // published, developing
